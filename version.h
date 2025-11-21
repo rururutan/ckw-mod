4aba8e421271371ddd06da4d0548b1026cc6de9f
Ru^3 <638143+rururutan@users.noreply.github.com>
2025-11-21 23:08:01 +0900

    Switch from Makefile to CMake

--- version.h
+++
@@ -1,8 +0,0 @@
-#define VERSION_MAJOR 0
-#define VERSION_MINOR 9
-#define VERSION_BUGFIX 1
-#define VERSION_STRING L"0.9.1"
-#define VERSION_NUMSTRING L"00901"
-#define BUILDDATE_STRING L"2022/12/09 16:59:54 UTC+9"
-#define COPYRIGHT_STRING L"Copyright (C) 2005-2022  Kazuo Ishii and ckw-mod creators and more..."
-#define ABOUT_DLG_STRING L"ckw-mod version 0.9.1 , build 2022/12/09 16:59:54 UTC+9\r\nCopyright (C) 2005-2022  Kazuo Ishii and ckw-moders"
